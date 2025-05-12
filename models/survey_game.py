e9c92201b0842e1f2a982a7727216c54c2f29351
MichaelVahos <michaelvahos2002@gmail.com>
2025-05-12 16:23:56 +0200

    boton iniciar juego en creación de sesiones

--- models/survey_game.py
+++ models/survey_game.py
@@ -12,8 +12,15 @@ class SurveyGameSession(models.Model):
         ('finished', 'Finished')
     ], default='waiting')
     participant_ids = fields.One2many('survey.game.participant', 'session_id', string='Participants')
+    current_question_id = fields.Many2one('survey.question', string='Current Question') 
 
     @api.model
     def _generate_game_code(self):
         return self.env['ir.sequence'].next_by_code('survey.game.session') or 'G0001'
 
+    def action_start_game(self):
+        for session in self:
+            first_question = session.survey_id.question_ids[:1]
+            if first_question:
+                session.current_question_id = first_question
+                session.state = 'in_progress'
