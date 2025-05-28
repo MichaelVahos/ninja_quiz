b1af488966cf318827e2e2a6efb04bf6cddd5866
MichaelVahos <michaelvahos2002@gmail.com>
2025-05-28 21:38:45 +0200

    fin de jornada 28/05, se hiceron comparaciones con un codigo nativo de odoo, pero sigue sin funcionar

--- controllers/kahoot_game_controller.py
+++ controllers/kahoot_game_controller.py
@@ -29,14 +29,14 @@ class KahootGameController(http.Controller):
     @http.route('/kahoot/game/data/<int:survey_id>', type='json', auth='public')
     def kahoot_game_data(self, survey_id, **kwargs):
         _logger.info(f"Entró al controlador /kahoot/game/data/{survey_id}")
-        survey = request.env['survey.survey'].sudo().browse(survey_id)
+        survey = request.env['survey.survey'].sudo().browse(survey_id) 
         
         # Verificar si la encuesta existe
         if not survey:
             return {'error': 'Encuesta no encontrada'}
         
         # Obtener la primera pregunta de la encuesta
-        question = survey.survey_question_ids[:1]  # Solo la primera pregunta
+        question = survey.survey_question_ids[:1]  #Solo la primera pregunta
         answers = [{'id': ans.id, 'value': ans.value} for ans in question.survey_answer_ids]
         
         return {
